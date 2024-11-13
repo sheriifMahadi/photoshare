6a4a9ee713afa6ef0baf632061b96e9142561dfd
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-11-13 21:10:20 +0100

    create template and views to list all users

--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib import messages
@@ -9,7 +9,9 @@ from .forms import (
     UserEditForm,
     ProfileEditForm)
 from .models import Profile
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 @login_required
 def dashboard(request):
@@ -99,4 +101,22 @@ def edit(request):
         'user_form': user_form,
         'profile_form': profile_form
         }
+    )
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request, 
+        'account/user/list.html',
+        {'section': 'people', 'users': users}
+    )
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request, 
+        'account/user/detail.html',
+        {'section': 'people', 'user': user}
     )
